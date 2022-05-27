d719a2e99069eb76045f5a03fce5cbbe443b416d
cares0 <cares00000@gmail.com>
2022-05-27 16:45:40 +0900

    V1 - JDK 동적 프록시 적용 - 로그 추적기

---
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,47 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.util.PatternMatchUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogTraceFilterHandler implements InvocationHandler {
+
+    private final Object target;
+    private final LogTrace logTrace;
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String[] patterns) {
+        this.target = target;
+        this.logTrace = logTrace;
+        this.patterns = patterns;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        // 메서드 이름 필터
+        String methodName = method.getName();
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) { // 패턴과 맞지 않으면
+            return method.invoke(target, args); // 로그를 찍지 않고 실제 객체의 메서드를 호출하도록
+        }
+
+        TraceStatus status = null;
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = logTrace.begin(message); // 클래스명.메서드명()
+
+            // 로직 호출
+            Object result = method.invoke(target, args);
+
+            logTrace.end(status);
+
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
